a0339a427102dc98edc9aaf2beff608f047a94ae
ciberliga4eso-hash <ciberliga4eso@gmail.com>
2026-02-12 19:14:54 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,20 @@
+
+var form = document.getElementById('loginForm');
+
+
+form.addEventListener('submit', function(e) {
+    
+
+    e.preventDefault();
+
+   
+    var usuario = document.getElementById('username').value;
+    var clave = document.getElementById('password').value;
+
+   
+    if (usuario === "Anonim123" && clave === "1234") {
+        window.location.href = "otra_pagina.html";
+    } else {
+        alert("Usuario o contraseña incorrectos");
+    }
+});
